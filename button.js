dd6936f770506e7372a5f356b1a7d2ca4221b026
anxinchubu <56666953+anxinchubu@users.noreply.github.com>
2024-04-18 13:53:18 +0800

    Update button.js

--- button.js
+++ button.js
@@ -1,11 +1,26 @@
 // button.js
-document.addEventListener("DOMContentLoaded", function() {
-  var button = document.querySelector(".custom-button");
-  button.addEventListener("click", function() {
+
+export default function initializeChatbot() {
+    Chatbot.init({
+        chatflowid: "04b252bf-0e77-416a-8340-eed035f7a8f9",
+        apiHost: "http://172.17.14.53:8088",
+        theme: {
+            button: {
+                backgroundColor: "green"
+            }
+        }
+    });
+}
+
+
+// button.js
+//document.addEventListener("DOMContentLoaded", function() {
+ // var button = document.querySelector(".custom-button");
+//  button.addEventListener("click", function() {
       // 这里添加按钮点击后的功能代码
-      alert("按钮被点击了！");
-  });
-});
+ //     alert("按钮被点击了！");
+ // });
+//});
 
 
 
